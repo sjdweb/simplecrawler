b36ab0f438012221832ee19f9d85335b06be0f75
Sean <sean@sjdev.co.uk>
2013-08-22 19:54:14 +0100

    Final touches

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Simplecrawler
 =============
 
 An uber simple crawler that returns pages local to the base url, along with the static assets they use.
+
+Usage
+----
+Output to console
+    python run.py http://domain.tld
+Output to file
+    python run.py http://domain.tld > output.txt
--- run.py
+++ run.py
@@ -1,15 +1,20 @@
+import sys
 import time
 import json
 from crawler.spyder import Spyder
 
 
-s = Spyder("http://gocardless.com")
+def main(argv):
+    print str.format("Crawling {0} as requested. Give me a minute!", argv)
+    spyder = Spyder(argv)
+    started = time.time()
+
+    result = spyder.run()
+    print json.dumps(result, indent=4, separators=(',', ': '))
+
+    finished = time.time() - started
+    print "Crawl took " + str(finished)
 
 
-started = time.time()
-
-result = s.run()
-print json.dumps(result, indent=4, separators=(',', ': '))
-
-finished = time.time() - started
-print "Crawl took " + str(finished)
+if __name__ == "__main__":
+    main(sys.argv[1])
